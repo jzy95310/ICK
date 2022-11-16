8c84a446cb15c6e1010502e748fa0f70472d6ece
Ziyang Jiang <ziyang.jiang@duke.edu>
2022-11-16 18:42:46 -0500

    Add X-learner benchmark and update ACIC challenge experiment

--- benchmarks/x_learner.py
+++ benchmarks/x_learner.py
@@ -5,6 +5,8 @@
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.linear_model import LogisticRegression
+from bartpy.sklearnmodel import SklearnModel
+from bartpy.initializers.sklearntreeinitializer import SklearnTreeInitializer
 
 class X_Learner_RF:
 
@@ -13,9 +15,9 @@ class X_Learner_RF:
         self.M2 = RandomForestClassifier(random_state=random_state) if classification else RandomForestRegressor(random_state=random_state)
         self.M3 = RandomForestClassifier(random_state=random_state) if classification else RandomForestRegressor(random_state=random_state)
         self.M4 = RandomForestClassifier(random_state=random_state) if classification else RandomForestRegressor(random_state=random_state)
-        self.g = LogisticRegression(max_iter=2000, random_state=0)
+        self.g = LogisticRegression(max_iter=2000, random_state=random_state)
     
-    def fit(self, X, T, Y):
+    def fit(self, X: np.ndarray, T: np.ndarray, Y: np.ndarray):
         # Step 1: Estimate the response and propensity functions
         X0 = X[(T == 0).ravel()]
         X1 = X[(T == 1).ravel()]
@@ -25,7 +27,7 @@ class X_Learner_RF:
         self.M2.fit(X1, Y1)
         self.g.fit(X, T.ravel())
 
-        # Step 2: Impute the treatment effect nad estimate the CATE function
+        # Step 2: Impute the treatment effect and estimate the CATE function
         D_hat = np.where((T == 0).ravel(), self.M2.predict(X) - Y.ravel(), Y.ravel() - self.M1.predict(X))
         self.M3.fit(X0, D_hat[(T == 0).ravel()])
         self.M4.fit(X1, D_hat[(T == 1).ravel()])
@@ -33,3 +35,12 @@ class X_Learner_RF:
     def predict(self, X):
         # Return the propensity weighted CATE
         return self.g.predict_proba(X)[:,0] * self.M3.predict(X) + self.g.predict_proba(X)[:,1] * self.M4.predict(X)
+
+class X_Learner_BART(X_Learner_RF):
+
+    def __init__(self, n_trees: int = 100, random_state: int = 0):
+        self.M1 = SklearnModel(n_trees=n_trees)
+        self.M2 = SklearnModel(n_trees=n_trees)
+        self.M3 = SklearnModel(n_trees=n_trees)
+        self.M4 = SklearnModel(n_trees=n_trees)
+        self.g = LogisticRegression(max_iter=2000, random_state=random_state)
