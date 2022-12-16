8386101d09d2e61042d899cca9b90769d643ef99
Ziyang Jiang <ziyang.jiang@duke.edu>
2022-12-16 16:52:25 -0500

    Add xray experiment

--- utils/helpers.py
+++ utils/helpers.py
@@ -2,19 +2,23 @@
 # SEE LICENSE STATEMENT AT THE END OF THE FILE
 
 import os
+from pathlib import Path
 import numpy as np
 import pickle as pkl
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import tarfile
+import urllib.request
 from scipy import stats
 from sklearn import metrics
 from torch import nn
-from typing import Callable, Dict, Tuple, List, Union
+from typing import Callable, Dict, Tuple, List, Union, Optional
 from kernels.constants import *
 from .constants import *
 from .data_generator import create_ick_data_generator
 from kernels.nn import ImplicitConvNet2DKernel, ImplicitNNKernel
+from google_drive_downloader import GoogleDriveDownloader as gdd
 
 def train_val_test_split(x: Union[List[np.ndarray], np.ndarray], y: np.ndarray, train_range: Tuple = (0.0,0.5), 
                          val_range: Tuple = (0.5,0.6), test_range: Tuple = (0.6,1.0), 
@@ -182,6 +186,106 @@ def attach_single_output_dense_layer(model: ImplicitNNKernel, activation: str = 
         model.dense_blocks.append(nn.Dropout(dropout_ratio))
         model.dense_blocks.append(nn.Linear(model.dense_blocks[-3].out_features, 1))
     return model
+
+def download_gdrive_if_needed(path: Path, file_id: str) -> None:
+    """
+    Helper for downloading a file from Google Drive, if it is now already on the disk.
+
+    Parameters
+    ----------
+    path: Path
+        Where to download the file
+    file_id: str
+        Google Drive File ID. Details: https://developers.google.com/drive/api/v3/about-files
+    """
+    path = Path(path)
+
+    if path.exists():
+        return
+
+    gdd.download_file_from_google_drive(file_id=file_id, dest_path=path)
+
+
+def download_http_if_needed(path: Path, url: str) -> None:
+    """
+    Helper for downloading a file, if it is now already on the disk.
+
+    Parameters
+    ----------
+    path: Path
+        Where to download the file.
+    url: URL string
+        HTTP URL for the dataset.
+    """
+    path = Path(path)
+
+    if path.exists():
+        return
+
+    if url.lower().startswith("http"):
+        urllib.request.urlretrieve(url, path)  # nosec
+        return
+
+    raise ValueError(f"Invalid url provided {url}")
+
+
+def unarchive_if_needed(path: Path, output_folder: Path) -> None:
+    """
+    Helper for uncompressing archives. Supports .tar.gz and .tar.
+
+    Parameters
+    ----------
+    path: Path
+        Source archive.
+    output_folder: Path
+        Where to unarchive.
+    """
+    if str(path).endswith(".tar.gz"):
+        tar = tarfile.open(path, "r:gz")
+        tar.extractall(path=output_folder)
+        tar.close()
+    elif str(path).endswith(".tar"):
+        tar = tarfile.open(path, "r:")
+        tar.extractall(path=output_folder)
+        tar.close()
+    else:
+        raise NotImplementedError(f"archive not supported {path}")
+
+
+def download_if_needed(
+    download_path: Path,
+    file_id: Optional[str] = None,  # used for downloading from Google Drive
+    http_url: Optional[str] = None,  # used for downloading from a HTTP URL
+    unarchive: bool = False,  # unzip a downloaded archive
+    unarchive_folder: Optional[Path] = None,  # unzip folder
+) -> None:
+    """
+    Helper for retrieving online datasets.
+
+    Parameters
+    ----------
+    download_path: str
+        Where to download the archive
+    file_id: str, optional
+        Set this if you want to download from a public Google drive share
+    http_url: str, optional
+        Set this if you want to download from a HTTP URL
+    unarchive: bool
+        Set this if you want to try to unarchive the downloaded file
+    unarchive_folder: str
+        Mandatory if you set unarchive to True.
+    """
+    if file_id is not None:
+        download_gdrive_if_needed(download_path, file_id)
+    elif http_url is not None:
+        download_http_if_needed(download_path, http_url)
+    else:
+        raise ValueError("Please provide a download URL")
+
+    if unarchive and unarchive_folder is None:
+        raise ValueError("Please provide a folder for the archive")
+    if unarchive and unarchive_folder is not None:
+        unarchive_if_needed(download_path, unarchive_folder)
 
 # ########################################################################################
 # MIT License
